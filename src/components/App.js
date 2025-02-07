c1a984937d52d6ba7c3872b2b5ba66d409037c64
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-07 14:39:03 -0500

    added handleChoice function to App.js - function will update morality category scores after user chooses from the two dilemma options, handleChoice also console.logs 'Game Over - Final Scores: newScore' when all of the dilemma questions have been asked

--- src/components/App.js
+++ src/components/App.js
@@ -13,13 +13,35 @@ function App() {
   //State for tracking user morality scores
   const [scores, setScores] = useState({utilitarian: 0, deontology: 0})
 
-
+  //State to track the current question index
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+  
   useEffect(retrieveDilemmas, [])
 
   function retrieveDilemmas(){
     fetch("http://localhost:4000/questions")
     .then(response => response.json())
     .then(setDilemmas)
+  }
+
+  //function will update the scores state based on user's choice
+  //function will also move to the next dilemma
+  //function will log the final scores when last question is answered
+  function handleChoice(categoryEffects) {
+    //updates the scores for each morality category after user chooses
+    setScores((prevScores) => {
+      const newScores = {
+        utilitarian: prevScores.utilitarian + (categoryEffects.utilitarian),
+        deontology: prevScores.deontology + (categoryEffects.deontology),
+      }
+
+      //Log the final scores when the last question is answered
+      if (currentQuestionIndex === dilemmas.length -1) {
+        console.log(`Game Over - Final Scores:${newScores}`)
+      }
+
+      return newScores
+    })
   }
 
   return (
@@ -29,7 +51,8 @@ function App() {
       <Outlet context={
         {
           dilemmas: dilemmas,
-          scores: scores
+          scores: scores,
+          currentQuestionIndex: currentQuestionIndex
         }
       }/>
       
