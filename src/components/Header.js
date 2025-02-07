6f8d77acaba64739f98656a385dd897d96e0c629
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-07 09:54:27 -0500

    Added db.json data

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+
+function Header() {
+    return(
+        <h1>the abyss gazes back</h1>
+    )
+}
+
+export default Header;
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+
+function NavBar(){
+    return(
+        <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/add_flight">Add Delimma</NavLink>
+            <NavLink to="/about">About You</NavLink>
+        </nav>
+    )
+}
+
+export default NavBar;
