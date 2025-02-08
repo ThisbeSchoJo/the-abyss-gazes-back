3ba3702c6f61184d10617bddd971870f39d87bd5
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-08 15:39:34 -0500

    created buttons for each choice and passed handleChoiceSelection to onClick so selected choice is console logged when clicked

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,7 +1,7 @@
 import { useOutletContext } from "react-router-dom";
 
 function Play(){
-    const { dilemmas, currentQuestionIndex } = useOutletContext()
+    const { dilemmas, currentQuestionIndex, onAnswered } = useOutletContext()
 
     const currentDilemma = dilemmas[currentQuestionIndex]
 
@@ -11,11 +11,20 @@ function Play(){
     else {
         return null
     }
+
+    //Handle when a uesr selects a choice
+    function handleChoiceSelection(choice) {
+        console.log(`Selected choice:${choice}`)
+        // onAnswered(choice) //call the callback to handle the answer
+    }
     
     return (
         <div key={currentDilemma.id}>
             <h1>{currentDilemma.question}</h1>
-            <h2>{currentDilemma.choices}</h2>
+            <div>
+                <button onClick={() => handleChoiceSelection(currentDilemma.choices[0])}>{currentDilemma.choices[0]}</button>
+                <button onClick={() => handleChoiceSelection(currentDilemma.choices[1])}>{currentDilemma.choices[1]}</button>
+            </div>
             {/* <h3>{currentDilemma.categoryEffects.utilitarian}</h3>
             <h3>{currentDilemma.categoryEffects.deontology}</h3> */}
         </div>
