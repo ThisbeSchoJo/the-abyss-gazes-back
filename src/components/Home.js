8e71cb2beb892266b78d96e30b9f2cebc73b75fb
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-10 10:02:42 -0500

    updated Home page text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 function Home(){
     return (
-        <h1>Will add home page text here - explain game/intro</h1>
+        <div>
+            <h3>Choose between the two evils.</h3>
+            <h5>"Battle not with monsters, lest ye become a monster, and if you gaze long into an abyss, the abyss also gazes into you." —Friedrich Nietzsche</h5>
+        </div>
+        
     )
 }
 
