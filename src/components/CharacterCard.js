730830f1aeaa14403f90265ca81bee467f7636d9
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-12 15:59:00 -0500

    added CharacterCard component and rendered it in CharacterList, added some character images to images folder

---
+++ src/components/CharacterCard.js
@@ -0,0 +1,11 @@
+function CharacterCard({ character }){
+    return(
+        <li>
+            <img src={character.image} alt={character.category}/>
+            <h2>{character.category}</h2>
+            <h2>Description: {character.description}</h2>
+        </li>
+    )
+}
+
+export default CharacterCard;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import CharacterCard from "./CharacterCard";
+
 function CharacterList(){
+    const [characters, setCharacters] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:4000/characters")
+        .then(response => response.json())
+        .then(setCharacters)
+    }, [])
+
     return (
-        <div>
-            
+        <div className="character-list">
+            <ul>
+                {characters.map((character) => {
+                    return <CharacterCard key={character.id} character={character}/>
+                })}
+            </ul>
         </div>
     )
 }
 
 export default CharacterList;
-
-
-
