aeb181fde28f2696dc7cb1712774e5b25548c8bf
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-12 16:05:42 -0500

    linked images in db.json to images folder for character cards, gave basic formatting in index.css

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,7 +1,7 @@
 function CharacterCard({ character }){
     return(
         <li>
-            <img src={character.image} alt={character.category}/>
+            <img className="character-photo" src={character.image} alt={character.category}/>
             <h2>{character.category}</h2>
             <h2>Description: {character.description}</h2>
         </li>
