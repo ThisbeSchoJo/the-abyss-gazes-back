9e184a320e7aaf33c684629b51d8d9b606c3d8a9
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-12 22:58:31 -0500

    adjusted text size on character cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,7 +3,7 @@ function CharacterCard({ character }){
         <div className="character-card">
             <img src={character.image} alt={character.category}/>
             <div className="character-info">
-                <h4>{character.category}</h4>
+                <h3>{character.category}</h3>
                 <h4>Description: {character.description}</h4>
             </div>
         </div>
